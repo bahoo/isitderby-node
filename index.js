47ffe75b055dd1e36c209fca4b4de1fa8e44bb11
bahoo <jon.c.culver@gmail.com>
2015-03-12 22:44:50 -0700

    Ooh node-based RSS feed yo.

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ app.get('/', function(request, response) {
   response.sendFile('index.html');
 });
 
+app.get('/feed', function(request, response){
+
+   var midnight = new Date();
+      midnight.setUTCHours(5,0,0,0);   // five hour offset from UTC
+
+   var description = 'No.';
+
+   var dateShort = (midnight.getMonth()+1) + '/' + (Math.floor((midnight.getDate()-1)/7)+1) + '/' + midnight.getDay();
+
+   if(dateShort == '5/1/6'){
+      description = 'YES, FRIENDS! YES! IT\'S THE DERBY! IT\'S THE GODDAMN KENTUCKY DERBY, GO AND TELL THE NEWS!';
+   }
+
+   var rssResponse = '<?xml version="1.0" encoding="ISO-8859-1"?>\n' +
+      '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n' +
+         '    <channel>\n' +
+            '        <title>Is It Derby?</title>\n' +
+            '        <link>http://www.isitderby.com/</link>\n' +
+            '        <atom:link href="http://isitderby.com/feed" rel="self" type="application/rss+xml" />\n' +
+            '        <description>Well, is it?</description>\n' +
+            '        <language>en-us</language>\n' +
+            '        <item>\n' +
+            '            <title>Is It Derby?</title>\n' +
+            '            <description>' + description + '</description>\n' +
+            '            <pubDate>' + midnight.toString() + '</pubDate>\n' +
+            '            <guid>http://www.isitderby.com/?' + midnight.getTime() + '</guid>\n' +
+         '        </item>\n' +
+         '    </channel>\n' +
+      '</rss>';
+
+   response.send(rssResponse);
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
